e505c6d95d32d475862609117f0567f2d960afcc
Nolan Hughes <nhughes987@gmail.com>
2017-10-26 22:35:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ def take_a_number(n, name)
   if n.length == 0
     puts "Welcome, #{name}. You are number 1 in line."
   else
-    puts "Welcome, #{name}. You are number 3 in line."
+    puts "Welcome, #{name}. You are number #{name[]} in line."
   end
 end
 
